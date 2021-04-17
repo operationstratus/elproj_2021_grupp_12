d682aa4b7790c621c735abc702ad9aa9d8379c0e
operationstratus <operationstratus@users.noreply.github.com>
2021-04-17 15:19:14 +0200

    Update

--- arduino_elproj/arduino_elproj.ino
+++ arduino_elproj/arduino_elproj.ino
@@ -3,17 +3,18 @@
 LiquidCrystal lcd(12,11,5,4,3,2); // generates an instance in the lcd
 
 const int switchPin = 13;
-const int contrastPin = 9;
+
+
 int switchState = 0;
 int prevSwitchState = 0;
 int reply;
 int counter = 0;
 int meny = 0;
 
+const int var = 0; // test variable
+
 void setup() {
   lcd.begin(16,2);
-  
-  pinMode(contrastPin, OUTPUT);
   /*
   lcd.print("Next: 05:00");
   lcd.setCursor(0,1); // changes the Cursor to continue writing in the second row
@@ -21,65 +22,30 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(contrastPin, 0);
 
   switchState = digitalRead(switchPin);
   if (switchState != prevSwitchState) {
     if (switchState == LOW) {
       lcd.clear();
-      
       switch(meny){
         case 0:
-        lcd.setCursor(0,0);
-        lcd.print("Next:");
-        lcd.print(counter);
-        lcd.setCursor(0,1);
-        lcd.print("Menue");
+        LCDprint(["Hej"]);
         break;
       }
     }
   }
   prevSwitchState = switchState;
+}
 
+void LCDprint(int row1[]){
+  char *line1 = "";
+  for (int i=0; i < sizeof(row1); i++){
+    line1 = line1 + String(row1[i]);
+  }
   
-
-  /*
-  if (switchState != prevSwitchState) {
-    if (switchState == LOW) {
-      reply = random(8);
-      lcd.clear(); // clears the writing
-      lcd.setCursor(0,0);
-      lcd.print("La bola dice:");
-      lcd.setCursor(0,1);
-
-      switch(reply){ // the program will enter the case assigned to the switch
-        case 0:
-        lcd.print("Si");
-        break;
-        case 1:
-        lcd.print("Es probable");
-        break;
-        case 2:
-        lcd.print("Ciertamente");
-        break;
-        case 3:
-        lcd.print("Buenas perspectivas");
-        break;
-        case 4:
-        lcd.print("No es seguro");
-        break;
-        case 5:
-        lcd.print("Pregunta de nuevo");
-        break;
-        case 6:
-        lcd.print("Ni idea");
-        break;
-        case 7:
-        lcd.print("No");
-        break;
-      }
-    }
-  }*/
   
-
+  lcd.setCursor(0,0);
+  lcd.print(line1);/*
+  lcd.setCursor(0,1);
+  lcd.print(line2);*/
 }
