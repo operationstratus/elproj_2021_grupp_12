096f63591186cf7ed020adc8e3e801b197d7d840
operationstratus <operationstratus@users.noreply.github.com>
2021-04-21 16:06:45 +0200

    Update arduino_elproj.ino

--- arduino_elproj/arduino_elproj.ino
+++ arduino_elproj/arduino_elproj.ino
@@ -14,8 +14,15 @@ String prevKeyState = ""; // key pressed in the last loop cycle
 String line0 = ""; // string to be printed on the first row on the lcd screen
 String line1 = ""; // string to be printed on the second row on the lcd screen
 
-int menueMain = 0; // inital switch state of menueMain
+int menuIntro = 0; // inital switch state of menuMain
+int menuMain = 0;
+int curMenuItem = 0;
 
+
+const int interval = 500;
+int prevTime = 0;
+
+String menuMainString[] = {"List alarms", "Set time", "Reset wheel", "Sound"};
 
 // Variabler som vi får tag på något sätt
 String nextTime = "12:40"; // från en klocka?
@@ -24,49 +31,40 @@ void setup() {
   Serial.begin(9600);
   lcd.begin(16,2);
 
-  
-  // initial screen print
-  line0 = " Next:  " + nextTime;
-  line1 = ">Menue";
-  myLCDprint(line0, line1);
+  menuWrite(menuMainString);
 }
 
 void loop() {
   //read the current pressed keyboard
   keyState = keyPressed();
-  
+  delay(15); // pausar systemet i 15ms, OBS vitalt för att undvika bounce-signaler och kalibalik.
+
   if (keyState != prevKeyState) {
     if (keyState != "N") {
-      lcd.clear();
-
-      
-      
-      switch(menueMain){
-        case 0:
-        line0 = "Next:  " + nextTime;
-        line1 = "Menue";
-        myLCDprint(line0, line1);
-        if (keyPressed() == "E") {
-          menueMain = 1;
-        }
-        break;
-
-        case 1:
-        line0 = "My times";
-        line1 = "Reset wheel";
-        myLCDprint(line0, line1);
-        if (keyPressed() == "L") {
-          menueMain = 0;
-        }    
-        break;
-
+      Serial.println(keyState);
+      if (keyState == "U") {
+        curMenuItem -= 1;
+        menuWrite(menuMainString);
+      }
+      if (keyState == "D") {
+        curMenuItem += 1;
+        //Serial.println(curMenuItem);
+        menuWrite(menuMainString);
       }
     }
-  }  
+  }
   prevKeyState = keyState;
 }
 
+void menuWrite(String menu[]) {
+  line0 = ">" + menu[0+curMenuItem];
+  line1 = " " + menu[1+curMenuItem];
+  //Serial.println(line0+"   "+line1);
+  myLCDprint(line0, line1);
+}
+
 void myLCDprint(String line0, String line1) {
+  lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(line0);
   lcd.setCursor(0,1);
