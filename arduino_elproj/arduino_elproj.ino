7ac6e7bc32fc9af0972ea9c026925e90905d8434
operationstratus <operationstratus@users.noreply.github.com>
2021-04-22 15:07:08 +0200

    Update arduino_elproj.ino

--- arduino_elproj/arduino_elproj.ino
+++ arduino_elproj/arduino_elproj.ino
@@ -6,10 +6,12 @@ const int switchPin = 13;
 const int kbdPin = A0;
 
 int kbdIn = 0; // pin for the keyboard
+String returnStr = ""; // variable inside readKBD() function
 
 
 String keyState = ""; // current key pressed: "E", "U", "D", "R", "L", or "N"
 String prevKeyState = ""; // key pressed in the last loop cycle
+
 
 String line0 = ""; // string to be printed on the first row on the lcd screen
 String line1 = ""; // string to be printed on the second row on the lcd screen
@@ -19,6 +21,7 @@ int menuMain = 0;
 int curMenuItem = 0;
 
 String menuMainString[] = {"List alarms", "Set time", "Reset wheel", "Sound"};
+String menuListAlarms[] = {"08:00", "12:30", "15:00", "18:45"};
 int leng = 0;
 
 // Variabler som vi får tag på något sätt
@@ -31,36 +34,39 @@ void setup() {
   // calculate the lenght of the current menu array and write the initial menu
   leng = sizeof(menuMainString)/sizeof(menuMainString[0]);
   menuWrite(menuMainString);
+
 }
 
 void loop() {
-  // calculate the lenght of the current menu array
-  leng = sizeof(menuMainString)/sizeof(menuMainString[0]);
-  
-  //read the current pressed keyboard
-  keyState = keyPressed();
-  delay(50); // pausar systemet i 15ms, OBS vitalt för att undvika bounce-signaler och kalibalik.
 
-  if (keyState != prevKeyState) {
-    if (keyState != "N") {
-      Serial.println(keyState);
-      if (keyState == "U" && curMenuItem > 0) {
-        curMenuItem -= 1;
-        menuWrite(menuMainString);
-      }
-      if (keyState == "D" && curMenuItem < leng-1) {
-        curMenuItem += 1;
-        //Serial.println(curMenuItem);
-        menuWrite(menuMainString);
-      }
+  readKBD(); // updates the keyState value
+  
+  
+  
+  if (keyState != prevKeyState && keyState != "N") {
+    Serial.println(keyState);
+
+    // calculate the lenght of the current menu array
+    leng = sizeof(menuMainString)/sizeof(menuMainString[0]);
+    
+    //Serial.println(keyState);
+    if (keyState == "U" && curMenuItem > 0) {
+      curMenuItem -= 1;
+      menuWrite(menuMainString);
     }
+    if (keyState == "D" && curMenuItem < leng-1) {
+      curMenuItem += 1;
+      //Serial.println(curMenuItem);
+      menuWrite(menuMainString);
+    }
+    
   }
   prevKeyState = keyState;
 }
 
 void menuWrite(String menu[]) {
   
-  Serial.println(curMenuItem);
+  //Serial.println(curMenuItem);
   line0 = ">" + menu[0+curMenuItem];
   if (curMenuItem < leng-1){
     line1 = " " + menu[1+curMenuItem];
@@ -80,7 +86,7 @@ void myLCDprint(String line0, String line1) {
   lcd.print(line1);
 }
 
-String keyPressed() {
+void readKBD() {
   kbdIn = analogRead(kbdPin);
   //Serial.println(kbdIn);
   /* Configuration:
@@ -90,27 +96,26 @@ String keyPressed() {
    * -------------------------- 
    * Analog value of output:
    * None: 1023
-   * Enter: 350-351
-   * Left: 165-166
-   * Right: 0
-   * Up: 87-88
-   * Down: 30-31
+   * Enter: 345-360
+   * Left: 165-175
+   * Right: 0-10
+   * Up: 85-95
+   * Down: 25-40
   */
+  
 
-  if (349 < kbdIn && kbdIn < 352) {
-    return "E";
+  if (345 < kbdIn && kbdIn < 360) {
+    returnStr = "E";
+  } else if (164 < kbdIn && kbdIn < 175) {
+    returnStr = "L";
+  } else if (0 < kbdIn && kbdIn < 10) {
+    returnStr = "R";
+  } else if (85 < kbdIn && kbdIn < 95) {
+    returnStr = "U";
+  } else if (25 < kbdIn && kbdIn < 40) {
+    returnStr = "D";
+  } else if (kbdIn == 1023) {
+    returnStr = "N";
   }
-  if (164 < kbdIn && kbdIn < 167) {
-    return "L";
-  }
-  if (0 == kbdIn) {
-    return "R";
-  }
-  if (86 < kbdIn && kbdIn < 89) {
-    return "U";
-  }
-  if (29 < kbdIn && kbdIn < 32) {
-    return "D";
-  }
-  return "N";
+  keyState = returnStr;
 }
