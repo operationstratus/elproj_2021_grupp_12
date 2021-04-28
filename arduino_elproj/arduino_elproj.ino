77c4db1c01dd16e58b89be178c11e8bee3d44120
operationstratus <operationstratus@users.noreply.github.com>
2021-04-28 16:10:29 +0200

    COMBINED CODE
    Have added Flaxels code for SD-card reading and Managing Alarms and reading current time from the clock

--- arduino_elproj/arduino_elproj.ino
+++ arduino_elproj/arduino_elproj.ino
@@ -1,3 +1,8 @@
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+////////////////////// SCREEN AND MENU SYSTEM ///////////////////////////////
+
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(9,8,7,6,5,4); // generates an instance in the lcd
@@ -22,10 +27,48 @@ String menuMainString[] = {"List alarms", "Set time", "Reset wheel", "Sound"};
 String menuListAlarms[] = {"08:00", "12:30", "15:00", "18:45"};
 int leng = 0;
 
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+////////////////////// CLOCK, SD READER, ALARM ///////////////////////////////
+
+// include the SD library:
+#include <SPI.h>
+#include <SD.h>
+//include RTC
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
+
+//time struct
+tmElements_t tm;
+
+//files
+String alarmFile = "alarms.txt";
+
+//variable wires
+const int chipSelect = 10;
+
+//variables
+String nextAlarmTime = "";
+String nextAlarmContent = "";
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////        SETUP        ///////////////////////////////
+
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16,2);
+  while (!Serial) ; // wait for Arduino Serial Monitor
+  delay(200);
 
+  // INIT THE ALARM AND SD
+  setRTC(15,4);
+  getNextAlarm();
+
+  // INIT THE LCD AND MENU VARS
+  lcd.begin(16,2);
   // calculate the lenght of the current menu array and write the initial menu
   leng = sizeof(menuMainString)/sizeof(menuMainString[0]);
   menuWrite(menuMainString);
@@ -39,7 +82,6 @@ void loop() {
   
   
   if (keyState != prevKeyState && keyState != "N") {
-    Serial.println(keyState);
 
     // calculate the lenght of the current menu array
     leng = sizeof(menuMainString)/sizeof(menuMainString[0]);
@@ -48,17 +90,29 @@ void loop() {
     if (keyState == "U" && curMenuItem > 0) {
       curMenuItem -= 1;
       menuWrite(menuMainString);
+      Serial.println("pressed: "+keyState);
     }
     if (keyState == "D" && curMenuItem < leng-1) {
       curMenuItem += 1;
       //Serial.println(curMenuItem);
       menuWrite(menuMainString);
+      Serial.println("pressed: "+keyState);
     }
     
   }
   prevKeyState = keyState;
+
+
+  ///////////////////////////////////////////////////// LOOP SD AND ALARM
+  if(getTime() == nextAlarmTime) alarm();
+  //delay(5000);
 }
 
+
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+/////////////////////// FUNCTIONS FOR SCREEN AND MENU /////////////////////////////
 
 
 void menuWrite(String menu[]) {
@@ -87,7 +141,7 @@ void myLCDprint(String line0, String line1) {
 void readKBD() {
   // ----------------------------------------READ KEYBOARD
   kbdIn = analogRead(kbdPin);
-  Serial.println(kbdIn);
+  // Serial.println(kbdIn);
   /* Configuration:
    *               Up
    * Enter    Left    Right
@@ -117,4 +171,139 @@ void readKBD() {
     returnStr = "N";
   }
   keyState = returnStr;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+/////////////////////// FUNCTIONS FOR SD, ALARM, CLOCK/////////////////////////////
+
+
+//checks all alarms on SD card and selects the next alarm to be executed
+//nextAlarmTime is set as next alarm and accompanying string is set in nextAlarmContent
+void getNextAlarm(){
+  String alarmString = readFromSD(alarmFile);
+  String tempTime = "";
+  nextAlarmTime = "23:59";
+  Serial.println(alarmString);
+  for(int i = 0; i < alarmString.length(); i++ ) {
+     if(alarmString[i] == '?'){
+      //compare times
+      tempTime.trim();
+      Serial.println(tempTime+" "+getTime()+" "+nextAlarmTime);
+      if(tempTime > getTime() && tempTime < nextAlarmTime){ //"smallest" time AFTER current time
+        nextAlarmTime = tempTime;
+        nextAlarmContent = alarmString.substring(i+1,alarmString.substring(i+1).indexOf('&')+i+1);
+      }
+     }
+     else if(alarmString[i] == '&'){
+      tempTime = "";
+     }
+     else {
+      tempTime += alarmString[i];
+     }
+  }
+  Serial.println("#"+nextAlarmTime+"#");
+  Serial.println("#"+nextAlarmContent+"#");
+}
+
+//activates the alarm and then calls getNextAlarm() after alarm has been deactivated
+void alarm(){
+  while(true){
+   Serial.println("ALARM!");
+   delay(1000);
+  }
+  getNextAlarm();
+}
+
+//reads fileName from SD card without delimiter
+//returns result as String built from individual characters from the SD card
+String readFromSD(String fileName){
+  Serial.println("ATTEMPTING CARD READ");
+  SD.begin(chipSelect);
+  File readFile = SD.open(fileName);
+  if(readFile){
+    String res = "";
+    while(readFile.available()){
+      char nextChar = char(readFile.read());
+      //EOF is reached
+      if(nextChar == '#'){
+        break;
+      }
+      res += nextChar;
+    }
+    readFile.close();
+    return res;
+  }
+  else{
+    Serial.println("Err: cannot open file (read): "+fileName);
+  }
+}
+
+//writes content+delimiter to fileName on SD card
+//previous content is deleted
+void writeToSD(String fileName, String content){
+  content += '#';
+  SD.begin(chipSelect);
+  //if file exists it is deleted to wipe content
+  if(SD.exists(fileName)) SD.remove(fileName);
+  File writeFile = SD.open(fileName, FILE_WRITE);
+  if(writeFile){
+    for(int i = 0; i < content.length(); i++ ) {
+      writeFile.print(content[i]);
+      //Making sure file isn't written to with duplicates, workaround for problem
+      if(content[i] == '#') break;
+    }
+  }
+  else {
+    Serial.println("Err: cannot open file (write): "+fileName);
+  }
+  writeFile.close();
+}
+
+//sets the time in tm and resets the second to 0
+//these values are then sent to the RTC
+bool setRTC(int newHour, int newMin){
+  tm.Hour = newHour;
+  tm.Minute = newMin;
+  //tm.Second = 0;
+  tm.Second = 50;
+  if (RTC.write(tm)) {
+      return true;
+    }
+  else {
+    return false;
+  }
+}
+
+//gets the time from the RTC and updates tm
+bool pollRTC(){
+  if (RTC.read(tm)) {
+    return true;
+  }
+  else return false;
+}
+
+//returns a String in the format hh:MM
+//leading zeros added to numbers n<10
+String getTime(){
+  if(!pollRTC()){
+    if (RTC.chipPresent()) {
+      //initialize clock
+      if(!setRTC){
+        Serial.println("ERR: RTC present but can't be set");
+      }
+    } else {
+      Serial.println("ERR: RTC not present");
+      return;
+    }
+  }
+  else {
+    //handle leading zeros and return
+    String tempHour = String(tm.Hour);
+    if (tm.Hour < 10) tempHour = '0'+tempHour;
+    String tempMinute = String(tm.Minute);
+    if (tm.Minute < 10) tempMinute = '0'+tempMinute;
+    return tempHour+':'+tempMinute;
+  }
 }
